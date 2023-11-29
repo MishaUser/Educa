401b95249bb6c1b85a89be4c1f7f2c01521c576c
misha <u-mi21@yandex.ru>
2023-11-29 23:57:35 +0300

    Переупорядочивание модулей и их содержимого

--- courses/views.py
+++ courses/views.py
@@ -11,6 +11,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 
 class OwnerMixin:
@@ -146,3 +147,24 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                    id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module': module})
+
+# позволяет обновлять порядок следования модулей курса
+class ModuleOrderView(CsrfExemptMixin,
+                      JsonRequestResponseMixin,
+                      View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                  course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
+
+class ContentOrderView(CsrfExemptMixin,
+                       JsonRequestResponseMixin,
+                       View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__course__owner=request.user) \
+                                   .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
